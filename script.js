8426b4e534d44f80a97ebcacf578cfa28e523b25
Mainak karmakar <karmakar_mainak@yahoo.com>
2024-01-18 13:48:45 +0530

    adding reCapCHA required

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 console.log("JS file for web to load");
-function formSubmitHandler(){
-    let outPutdate = document.querySelector(".outPutdate");
-    let inPutDate = document.getElementById("00N5g00000hktmC");
-    console.log(typeof inPutDate.value);
-    //Now change String - date (en_IN)
-    outPutdate.value=new Date(inPutDate.value).toLocaleDateString("en-IN");  
+function formSubmitHandler(event){
+    if(capchaChecked){
+        let outPutdate = document.querySelector(".outPutdate");
+        let inPutDate = document.getElementById("00N5g00000hktmC");
+        console.log(typeof inPutDate.value);
+        //Now change String - date (en_IN)
+        outPutdate.value=new Date(inPutDate.value).toLocaleDateString("en-IN");  
+    }
+    else{
+        alert("Please check the reCAPCHA to Submit");
+        event.preventDefault();
+    }
+
 }
 function handelChange(){
     console.log('inside handelChange');
@@ -22,3 +29,7 @@ var response = document.getElementById("g-recaptcha-response");
 if (response == null || response.value.trim() == "") {
     var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
 } } setInterval(timestamp, 500); 
+let capchaChecked =false;
+function capchasuccess(){
+    capchaChecked =true;
+}
